8f0b1e5bc4d8955d62f7ffb39f5dc277b7a73b45
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2021-11-23 16:03:37 +0900

    hiro/implement_routing_init

---
+++ users/user.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func CreateUser(ctx echo.Context) error {
+	return ctx.String(http.StatusOK, "successfully create user")
+}
+
+func EditUser(ctx echo.Context) error {
+	id := ctx.Param("id")
+	msg := "successfully edit user id:" + id
+	return ctx.String(http.StatusOK, msg)
+}
+
+func GetUser(ctx echo.Context) error {
+	id := ctx.Param("id")
+	msg := "successfully get user id:" + id
+	return ctx.String(http.StatusOK, msg)
+}

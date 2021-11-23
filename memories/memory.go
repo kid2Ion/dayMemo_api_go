a81a9f906a0315c29638aa7b065c2292b61965b7
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2021-11-23 16:03:37 +0900

    hiro/implement_routing_init

---
+++ memories/memory.go
@@ -0,0 +1,29 @@
+package memory
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func CreateMemory(ctx echo.Context) error {
+	return ctx.String(http.StatusOK, "successfully create memory")
+}
+
+func EditMemory(ctx echo.Context) error {
+	id := ctx.Param("id")
+	msg := "successfully edit memory id:" + id
+	return ctx.String(http.StatusOK, msg)
+}
+
+func GetMemoriesList(ctx echo.Context) error {
+	year_month := ctx.QueryParam("year_month")
+	msg := "successfully get memories list year_month:" + year_month
+	return ctx.String(http.StatusOK, msg)
+}
+
+func GetMemory(ctx echo.Context) error {
+	id := ctx.Param("id")
+	msg := "successfully get memory id:" + id
+	return ctx.String(http.StatusOK, msg)
+}

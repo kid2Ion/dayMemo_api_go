219d19855bd9162ac06ab8d5906df3fcb7d0aefd
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2021-12-04 16:48:22 +0900

    create_db

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	cloud.google.com/go/firestore v1.6.1 // indirect
 	firebase.google.com/go v3.13.0+incompatible
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/jinzhu/gorm v1.9.16
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.1 // indirect
 	google.golang.org/api v0.60.0
--- model/memories.go
+++ model/memories.go
@@ -1,15 +1,48 @@
 package model
 
-import "time"
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Memory struct {
-	ID int `json:"id" gorm:"praimaly_key"`
-	UserId string `json:"user_id"`
-	content string `json:"content"`
+	gorm.Model
+	UID      int    `json:"uid"`
+	ID       int    `json:"id" gorm:"praimaly_key"`
+	Content  string `json:"content"`
 	ImageUrl string `json:"image_url"`
-	CreatedAt time.Time
 }
 
 type MemoryList []Memory
 
-func Create
+func CreateMemory(memory *Memory) {
+	db.Create(memory)
+}
+
+func FindMemory(m *Memory) MemoryList {
+	var memoryList MemoryList
+	db.Where(m).Find(&memoryList)
+
+	return memoryList
+}
+
+func DeleteMemory(m *Memory) error {
+	if rows := db.Where(m).Delete(&Memory{}).RowsAffected; rows == 0 {
+		return fmt.Errorf("could not find memory:%v to delete", m)
+	}
+
+	return nil
+}
+
+func UpdateMemory(m *Memory) error {
+	rows := db.Model(m).Update(map[string]interface{}{
+		"content":  m.Content,
+		"imageurl": m.ImageUrl,
+	}).RowsAffected
+	if rows == 0 {
+		return fmt.Errorf("could not find memory: %v", m)
+	}
+
+	return nil
+}

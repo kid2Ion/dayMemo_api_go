e9ae0b4b77722d0e29067da63df762b302bc60ad
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-01-03 01:55:39 +0900

    validation_user

--- user/user.go
+++ user/user.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hiroki-kondo-git/dayMemo_api_go/auth"
 	"github.com/hiroki-kondo-git/dayMemo_api_go/model"
 	"github.com/labstack/echo"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 func Signup(ctx echo.Context) error {
@@ -23,10 +24,13 @@ func Signup(ctx echo.Context) error {
 	}
 	user.ID = uid
 
-	if user.Password == "" {
+	// validation
+	validate := validator.New()
+	errors := validate.Struct(user)
+	if errors != nil {
 		return &echo.HTTPError{
 			Code:    http.StatusBadRequest,
-			Message: "invalid or password",
+			Message: ValidationError(errors),
 		}
 	}
 
@@ -62,7 +66,6 @@ func GetUser(ctx echo.Context) error {
 
 func UpdateUser(ctx echo.Context) error {
 	user := new(model.User)
-	// todo validation
 	if err := ctx.Bind(user); err != nil {
 		return err
 	}
@@ -72,6 +75,23 @@ func UpdateUser(ctx echo.Context) error {
 		return err
 	}
 	user.ID = uid
+
+	if u := model.FindUser(user); u.ID != "" {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
+			Message: "name already exists",
+		}
+	}
+
+	// validation
+	validate := validator.New()
+	errors := validate.Struct(user)
+	if errors != nil {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
+			Message: ValidationError(errors),
+		}
+	}
 
 	user = model.UpdateUser(user)
 
@@ -88,4 +108,32 @@ func DeleteUser(ctx echo.Context) error {
 
 	model.DeleteUser(user)
 	return ctx.JSON(http.StatusOK, "successfully user delete")
+}
+
+// validationErrMessage
+func ValidationError(err error) []string {
+	var errorMessages []string
+	for _, err := range err.(validator.ValidationErrors) {
+		var errorMessage string
+		fieldName := err.Field()
+
+		switch fieldName {
+		case "UserName":
+			var typ = err.Tag()
+			switch typ {
+			case "min":
+				errorMessage = "username must over 4 charactors"
+			case "max":
+				errorMessage = "username must less 10 charactors"
+			case "alphanum":
+				errorMessage = "username must use only alpha and number"
+			}
+		case "DisplayName":
+			errorMessage = "displayName must over 1 less 10 charactors"
+		case "Password":
+			errorMessage = "password must over 6 charactors"
+		}
+		errorMessages = append(errorMessages, errorMessage)
+	}
+	return errorMessages
 }
